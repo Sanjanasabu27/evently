8104c7b54c882c98a658050d58d3d07c7e89bf0b
Your Name <sanjanasabu007@gmail.com>
2026-07-15 21:51:07 +0530

    Configure MongoDB Atlas and prepare for deployment

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/eventdb")
-
-.then(() => {
+mongoose
+  .connect(
+    "mongodb://eventlyadmin:Event12345@ac-2zpjco1-shard-00-00.aykqqs9.mongodb.net:27017,ac-2zpjco1-shard-00-01.aykqqs9.mongodb.net:27017,ac-2zpjco1-shard-00-02.aykqqs9.mongodb.net:27017/eventdb?ssl=true&replicaSet=atlas-2ytcg7-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
     console.log("Database Connected");
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
